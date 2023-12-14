dc634b30141b633e01f94babcc4db637961079aa
Aleksey27007 <alekseyvaruj@gmail.com>
2023-12-14 22:05:39 +0300

    Created new file MinIndexElement

---
+++ src/main/java/HomeWork11_12_23/MinIndexElement.java
@@ -0,0 +1,7 @@
+package HomeWork11_12_23;
+
+public class MinIndexElement {
+    public static void main(String[] args) {
+        /* Найти минимальный из элементов массива с нечетными индексами */
+    }
+}
