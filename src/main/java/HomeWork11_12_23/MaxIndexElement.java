82f2f18ca25e0e935388c7b30b68f830f3bc9e38
Aleksey27007 <alekseyvaruj@gmail.com>
2023-12-14 22:04:30 +0300

    Finished MaxIndexElement

--- src/main/java/HomeWork11_12_23/MaxIndexElement.java
+++ src/main/java/HomeWork11_12_23/MaxIndexElement.java
@@ -1,7 +1,28 @@
 package HomeWork11_12_23;
 
+import static HomeWork11_12_23.MaxNumberArray.print;
+import static HomeWork11_12_23.SumDigitsArray.randomArray;
+
 public class MaxIndexElement {
     public static void main(String[] args) {
         /* Найти максимальный из элементов массива с четными индексами */
+
+        int[] array = randomArray(10);
+        print(array);
+
+        int maxNumber = 0;
+        int[] indexArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 2; i < array.length; i++) {
+            if (i % 2 == 0) {
+                if (array[i] > maxNumber) {
+                    maxNumber = array[i];
+                }
+            } else {
+                continue;
+            }
+        }
+
+        System.out.println("Max even number = " + maxNumber);
+        print(indexArray);
     }
 }
