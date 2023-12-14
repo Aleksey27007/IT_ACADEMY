8a5cc470c69e42296db4c7411498031c5074c964
Aleksey27007 <alekseyvaruj@gmail.com>
2023-12-14 13:31:16 +0300

    Finished SumDigitsArray added public method randomArray and print

--- src/main/java/HomeWork11_12_23/SumDigitsArray.java
+++ src/main/java/HomeWork11_12_23/SumDigitsArray.java
@@ -7,7 +7,7 @@ import static HomeWork11_12_23.MaxNumberArray.print;
 public class SumDigitsArray {
     public static void main(String[] args) {
         /* Найти сумму цифр массив */
-        int[] array = RandomArray(20);
+        int[] array = randomArray(20);
         print(array);
 
         int sum = 0;
@@ -17,7 +17,7 @@ public class SumDigitsArray {
         System.out.println("Sum digits = " + sum);
     }
 
-    public static int[] RandomArray(int numberDigits) {
+    public static int[] randomArray(int numberDigits) {
         Random random = new Random();
         int[] array = new int[numberDigits];
 
