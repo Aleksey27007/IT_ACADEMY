ae9effb2b65fe88327df3d50b3a8069fd1679bcc
Aleksey27007 <alekseyvaruj@gmail.com>
2023-12-14 22:32:08 +0300

    Added cicle for and random for positive and negative numbers PlusMinusArrays

--- src/main/java/HomeWork11_12_23/PlusMinusArrays.java
+++ src/main/java/HomeWork11_12_23/PlusMinusArrays.java
@@ -1,4 +1,19 @@
 package HomeWork11_12_23;
 
+import java.util.Random;
+
+import static HomeWork11_12_23.MaxNumberArray.print;
+
 public class PlusMinusArrays {
+    public static void main(String[] args) {
+        /* Разложить положительные и отрицательные числа по разным массивам */
+
+        Random random = new Random();
+        int[] mainArray = new int[20];
+
+        for (int i = 0; i < mainArray.length; i++) {
+            mainArray[i] = random.nextInt(20) - 10;
+        }
+        print(mainArray);
+    }
 }
