04fe9a480f353c98af384b1e96d809a850f952ae
Aleksey27007 <alekseyvaruj@gmail.com>
2023-12-14 21:56:28 +0300

    Created new file MaxIndexElement.java and MassiveHW

---
+++ src/main/java/HomeWork11_12_23/MaxIndexElement.java
@@ -0,0 +1,7 @@
+package HomeWork11_12_23;
+
+public class MaxIndexElement {
+    public static void main(String[] args) {
+        /* Найти максимальный из элементов массива с четными индексами */
+    }
+}
