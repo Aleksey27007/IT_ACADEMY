2f89acf0609760972ac38afdfe2879da5fdaf05f
Aleksey27007 <alekseyvaruj@gmail.com>
2023-12-15 19:23:08 +0300

    finished hw lusinusarray

--- src/main/java/HomeWork11_12_23/PlusMinusArrays.java
+++ src/main/java/HomeWork11_12_23/PlusMinusArrays.java
@@ -10,10 +10,42 @@ public class PlusMinusArrays {
 
         Random random = new Random();
         int[] mainArray = new int[20];
+        int countPositive = 0;
+        int countNegative = 0;
 
         for (int i = 0; i < mainArray.length; i++) {
             mainArray[i] = random.nextInt(20) - 10;
         }
+
         print(mainArray);
+
+        for (int i = 0; i < mainArray.length ; i++) {
+            if (mainArray[i] > 0) {
+                countPositive++;
+            } else {
+                countNegative++;
+            }
+        }
+
+        int[] arrayPositive = new int[countPositive];
+        int[] arrayNegative = new int[countNegative];
+        int tempPositeve = 0;
+        int tempNegetive = 0;
+
+        for (int i = 0; i < mainArray.length; i++) {
+            if (mainArray[i] > 0) {
+                arrayPositive[i - tempPositeve] = mainArray[i];
+                tempNegetive++;
+            } else {
+                arrayNegative[i - tempNegetive] = mainArray[i];
+                tempPositeve++;
+            }
+        }
+
+        System.out.println("\nPositive array: ");
+        print(arrayPositive);
+
+        System.out.println("\nNegative array: ");
+        print(arrayNegative);
     }
 }
