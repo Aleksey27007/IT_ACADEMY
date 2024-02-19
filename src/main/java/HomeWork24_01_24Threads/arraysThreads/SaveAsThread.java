046db9aba8e575fafed8c080282fd8dd2c504b0d
Aleksey27007 <alekseyvaruj@gmail.com>
2024-02-19 19:03:08 +0300

    hw 24_01_24 fixed one exercise arraysThread

--- src/main/java/HomeWork24_01_24Threads/arraysThreads/SaveAsThread.java
+++ src/main/java/HomeWork24_01_24Threads/arraysThreads/SaveAsThread.java
@@ -7,7 +7,7 @@ public class SaveAsThread extends Thread {
     private final static String PATH = "D:\\IntJava\\IT_Academy\\IT_AcademyLessonsAndHomeW\\src\\main\\java\\HomeWork24_01_24Threads\\arraysThreads\\";
     private final static String FILE_NAME = "your_arrays.txt";
     private final static String ABSOLUTE_PATH = PATH + FILE_NAME;
-    private Object obj = new Object();
+
     private int[] array;
 
     public SaveAsThread(int[] array) {
@@ -20,7 +20,7 @@ public class SaveAsThread extends Thread {
     @Override
     public void run() {
         File file = new File(ABSOLUTE_PATH);
-        synchronized (obj) {
+        synchronized (SaveAsThread.class) {
             try(final BufferedWriter bf = new BufferedWriter(new FileWriter(file, true))) {
                 for (int element : array) {
                     bf.write(Integer.toString(element));
